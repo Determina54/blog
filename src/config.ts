3bea98831df8940b7652c97a54cc4f295f8194e9
Di Wusi <Determination54@outlook.com>
2025-12-26 22:16:29 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Determina54",
 	bio: "一切都将逝去，唯有死神永生",
 	links: [
